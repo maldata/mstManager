f6d60ef7f24d488ee8ed5e57febe7ac9a97e414d
maldata <maldata@gmail.com>
2016-12-08 22:38:40 -0600

    - Set up the schema for the episodes and seasons tables

---
+++ models/seasons.py
@@ -0,0 +1,22 @@
+import models.modelBase
+import models.episodes
+import sqlalchemy
+import sqlalchemy.orm
+
+
+class Season(models.modelBase.Base):
+    # Here we define our class variables. These are shared among all instances of this class.
+    # They can be accessed as (for example) Address.id from inside the class or outside the class.
+    __tablename__ = 'seasons'
+
+    # Among our class variables are the descriptions of each column of the table, as follows
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    seasonNumber = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    # A season contains multiple episodes. Each record in the episodes table will have a foreign key
+    # to a season, so here we'll have a collection of episodes in this season.
+    episodes = sqlalchemy.orm.relationship("Episode", order_by=models.episodes.Episode.id,
+                                           back_populates="season")
+
+    def __repr__(self):
+        return "<Season(seasonNum='%s')>" % self.seasonNumber
