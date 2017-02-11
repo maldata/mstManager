7eb555a1ec54c56eecb013d9270512e5df07341a
maldata <maldata@gmail.com>
2017-02-11 00:44:22 -0600

    - Continuing to add to db engine

--- mstmanager/db/engine.py
+++ mstmanager/db/engine.py
@@ -32,7 +32,8 @@ class DbEngine:
 
     def add_or_get_season(self, number):
         s = self._session.query(models.Season) \
-                         .filter(models.Season.number == number).one_or_none()
+                         .filter(models.Season.number == number) \
+                         .one_or_none()
 
         if s is None:
             s = models.Season(number=number)
@@ -44,15 +45,41 @@ class DbEngine:
     def get_all_seasons(self):
         return self._session.query(models.Season).all()
 
+    def get_all_season_numbers(self):
+        ss = self.get_all_seasons()
+        return [s.number for s in ss]
+
+    def get_season(self, number):
+        self._session.query(models.Season) \
+                     .filter(models.Season.number == number) \
+                     .one_or_none()
+        
     def parse_episode_number(self, number):
-        # TODO
-        season = 'K'
-        episode_number = 7
-        return (season, episode_number)
+        """
+        Break up an episode number (string) into the season identifier and
+        the episode number (int)
+        :param number: the full episode number to parse
+        :type number: str
+        """
+        try:
+            season = number[:-2]
+            ep = number[-2:]
+            episode_number = int(ep)
+            return (season, episode_number)
+        except ValueError:
+            return None
 
     def build_episode_number(self, season, episode_number):
-        # TODO
-        return 'K07'
+        """
+        Put together the season string and an episode number (integer)
+        into the full episode id
+        :param season: the season id string
+        :type season: str
+        :param episode_number: the number of the episode within the season
+        :type episode_number: int
+        """
+        episode_string = '{:02d}'.format(episode_number)
+        return season + episode_string
     
     def add_episode(self, number, name):
         """
@@ -61,8 +88,21 @@ class DbEngine:
         :param name: the episode name
         :type name: str, e.g., 'Gamera vs. Zigra'
         """
-        # TODO
-        # parse the episode number
-        # check if the season exists
-        # if not, add it
-        e = models.Episode(number=number, name=name)
+        (season, episode) = self.parse_episode_number(number)
+        s = self.add_or_get_season(season)
+        
+        e = models.Episode(number=episode, name=name)
+        e.season = s
+        
+        self._session.add(e)
+        self._session.commit()
+
+        return e
+
+    def add_media_set(self, name):
+        m = models.MediaSet(name=name)
+        self._session.add(m)
+        self._session.commit()
+
+        return m
+
--- tests/test_db.py
+++ tests/test_db.py
@@ -2,6 +2,7 @@ from context import *
 
 import unittest
 import mstmanager.db.engine as db
+import mstmanager.db.models as models
 
 
 class EmptyDbTestSuite(unittest.TestCase):
@@ -26,6 +27,26 @@ class EmptyDbTestSuite(unittest.TestCase):
         self.assertNotEqual(s1, s2)
         self.assertEqual(len(self.engine.get_all_seasons()), 2)
 
+    def test_add_episode(self):
+        e1 = self.engine.add_episode('K07', 'Gamera vs. Zigra')
+        es = self.engine._session.query(models.Episode).all()
+        self.assertEqual(len(es), 1)
+        self.assertEqual(e1, es[0])
+
+    def test_add_media_set(self):
+        m1 = self.engine.add_media_set('DAP DVD')
+        ms = self.engine._session.query(models.MediaSet).all()
+        self.assertEqual(len(ms), 1)
+        self.assertEqual(m1, ms[0])
+
+    def test_get_seasons(self):
+        s1 = self.engine.add_or_get_season('K')
+        s2 = self.engine.add_or_get_season('1')
+        s3 = self.engine.add_or_get_season('K')
+        s4 = self.engine.add_or_get_season('2')
+
+        self.assertEqual(self.engine.get_all_season_numbers(), ['K', '1', '2'])
+        
 
 class PopulatedDbTestSuite(unittest.TestCase):
     def setUp(self):
